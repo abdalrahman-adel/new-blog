5ca0d6690a894d17e9023b1af0db06f7ebbb26c5
[abdalrahman.adel] <[abdalrahmanadel380@gmail.com]>
2021-02-04 23:17:46 +0200

    after adding auth

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+}) ->name('welcome');
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
